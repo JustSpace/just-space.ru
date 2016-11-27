6d254d84ea464c5b675dd8ddfa551f5ab49776f5
akpoflash <akpoflash@gmail.com>
2016-11-27 12:48:23 +0300

    Fix size of scale and fix bug in tools.php.

--- tools.php
+++ tools.php
@@ -2,7 +2,8 @@
   require_once($_SERVER["DOCUMENT_ROOT"]."/templates/doc_head.php");
 ?>
 <body>
-  <header class="header header__tools">
+  <header class="header header--tools">
+    <div class="header__mask"></div>
     <h1 class="header__title header__title--slice">Наши инструменты</h1>
     <nav class="navigation">
       <a href="javascript:void(0)" class="hamburger">
