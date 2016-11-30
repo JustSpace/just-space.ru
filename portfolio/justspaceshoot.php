8088fba4fb92f371dedf5e595c2ebce3885a59e3
akpoflash <akpoflash@gmail.com>
2016-11-30 19:41:31 +0300

    Add text in the justspaceshoot.

--- portfolio/justspaceshoot.php
+++ portfolio/justspaceshoot.php
@@ -23,7 +23,57 @@
         Создание 8-и битной мобильной игры
       </h1>
       <section class="about__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed aut temporibus aspernatur architecto rem ipsa corrupti nobis, maiores laudantium rerum nulla earum neque, numquam cum possimus quae deserunt! Illum voluptatem quam a nulla mollitia pariatur obcaecati iure, numquam autem officiis quaerat, eligendi at asperiores laudantium eos magnam ducimus dolorem. Sint porro debitis tenetur natus obcaecati neque culpa aut dolore quae tempora, facilis cumque est quasi, ad accusantium iste in asperiores perspiciatis dolorum ducimus fugit assumenda sequi. Eaque voluptatibus deleniti quo numquam unde quam quod earum, sunt, expedita beatae totam tenetur aliquid? Tenetur cum similique molestias earum. Repudiandae mollitia ex quisquam voluptatibus voluptatum placeat similique autem distinctio non iusto alias totam, cupiditate assumenda, cumque commodi. Ex tempora sequi tempore saepe similique quod in fuga libero quia, suscipit, incidunt voluptatum officiis vero optio perferendis fugiat ducimus. Reprehenderit necessitatibus repudiandae eos nulla hic dolorum voluptatem maiores odio eaque sequi, temporibus, quasi praesentium et, ea aliquam, repellat omnis quo! Vel quam aliquid eius maiores autem, id consectetur placeat debitis suscipit error. Placeat iste, quibusdam error, iusto nemo provident. Minus rem reiciendis ipsa excepturi sapiente adipisci beatae atque veritatis omnis libero fuga assumenda similique provident, molestiae consequatur totam voluptates ducimus deleniti pariatur quo labore eos quia delectus. Totam molestiae perspiciatis optio numquam quibusdam quis eaque quia vel iste, aspernatur. Fugit, soluta qui eligendi asperiores reprehenderit repudiandae reiciendis quidem. Architecto earum corporis suscipit fugit minima ipsum, quibusdam praesentium ab numquam amet! Mollitia quidem cum modi nesciunt nemo? Quas nihil distinctio nulla dicta placeat minima, necessitatibus minus doloremque, temporibus, animi, odio autem modi quia possimus repudiandae tenetur sed impedit dolore explicabo accusantium ipsa! Doloribus rem ipsam asperiores quas, dignissimos hic deleniti. Eaque provident minus eos officia perferendis aliquid impedit sequi modi non hic inventore unde dolor, necessitatibus numquam ratione voluptate, debitis in pariatur obcaecati! Aliquid autem vitae facilis porro repellat, error doloremque alias perspiciatis, possimus dicta? Vero debitis omnis voluptates odio repudiandae in libero sequi dolore repellat laborum quidem quisquam facere veniam sapiente vel aliquid minus commodi sint, perspiciatis fugit magni voluptate. Iusto nesciunt reiciendis, et consequatur repellendus non provident, quam amet quidem ipsa adipisci eos quas facilis ex sapiente, dolore quisquam magnam omnis explicabo. Odio soluta laboriosam recusandae. Iusto nobis repellendus, distinctio ab aliquid tempora saepe neque deserunt tempore ut fuga, quis totam facere et temporibus? Aliquid ipsum, rerum placeat odit? Omnis provident fugiat repellat nisi aut quae itaque deleniti, culpa unde officiis nobis adipisci quos.
+        На данный момент аркадные игры занимают одну из лидирующих позиций App Store,
+        поэтому и мы решили попробовать свои силы в данном направлении.
+
+        Основной идеей было воплотить классический шутер в стиле 8-bit на эране соверменного мобильного устройства, вам предлагается управлять различными космическими кораблями с целью убить наибольшее количество противников за неограниченное время, при этом противники в свою очередь препятствуют вашему продвижению.
+      </section>
+      <section class="about__text">
+        <ul>
+          <li>
+            - Дизайн объектов для наполнения игровой сцены
+          </li>
+          <li>
+            - Поиск звуков
+          </li>
+          <li>
+            - Проектирование интерфейса пользователя
+          </li>
+          <li>
+            - Разработка игровой физики на основе Sprite Kit
+          </li>
+          <li>
+            - Наполнение сцены объектами, подготовленными дизайнером
+          </li>
+          <li>
+            - Дизайн иконки приложения, соответствующие скриншоты для App Store
+          </li>
+          <li>
+            - Подбор ключевых слов для поиска по App Store
+          </li>
+        </ul>
+      </section>
+      <section class="about__text">
+        При разработке интерфейса и игровых объектов дизайнер подготовил множество различных решений. Выбрав наиболее удачное
+        решение, мы выполнили несложное прототипирование интерфейсов.
+         Утвержденная задача была представлена iOS разработчику для непосредственного проектирования. В качестве движка был
+        выбран Sprite Kit. Поскольку разработчики создавали его заточенным специально для устройств Apple, он существенно выигрывает
+        в скорости у других движков.
+         Параллельно работе разработчика, дизайнер подготовил звуковые эффекты, иконку для приложения и скриншоты
+        для App Store.
+         После совмещения работы разработчика и дизайнерского решения оставалось лишь подобрать ключевые слова для более
+        успешного продвижения.
+      </section>
+      <section class="about__text">
+        При разработке интерфейса и игровых объектов дизайнер подготовил множество различных решений. Выбрав наиболее удачное
+        решение, мы выполнили несложное прототипирование интерфейсов.
+         Утвержденная задача была представлена iOS разработчику для непосредственного проектирования. В качестве движка был
+        выбран Sprite Kit. Поскольку разработчики создавали его заточенным специально для устройств Apple, он существенно выигрывает
+        в скорости у других движков.
+         Параллельно работе разработчика, дизайнер подготовил звуковые эффекты, иконку для приложения и скриншоты
+        для App Store.
+         Следующим этапом оставалось провести оптимизацию ключевых слов, используя собственные наработанные решения.
+         Все исходники нашего проекта находятся в свободном доступе на github, что позволяет пользоваться нашими готовыми решениями.
       </section>
     </article>
   </main>
