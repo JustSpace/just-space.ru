2bb814178f1cec8af935b69b5f42b984095b293b
akpoflash <akpoflash@gmail.com>
2016-12-08 09:50:54 +0300

    Add main steps in the develop.

--- portfolio/doly-tula.php
+++ portfolio/doly-tula.php
@@ -20,10 +20,26 @@
   <main class="main">
     <article class="about">
       <h1>
-        Создание сайта визитки под ключ
+        Создание сайта корпоративного сайта под ключ
       </h1>
       <section class="about__text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed aut temporibus aspernatur architecto rem ipsa corrupti nobis, maiores laudantium rerum nulla earum neque, numquam cum possimus quae deserunt! Illum voluptatem quam a nulla mollitia pariatur obcaecati iure, numquam autem officiis quaerat, eligendi at asperiores laudantium eos magnam ducimus dolorem. Sint porro debitis tenetur natus obcaecati neque culpa aut dolore quae tempora, facilis cumque est quasi, ad accusantium iste in asperiores perspiciatis dolorum ducimus fugit assumenda sequi. Eaque voluptatibus deleniti quo numquam unde quam quod earum, sunt, expedita beatae totam tenetur aliquid? Tenetur cum similique molestias earum. Repudiandae mollitia ex quisquam voluptatibus voluptatum placeat similique autem distinctio non iusto alias totam, cupiditate assumenda, cumque commodi. Ex tempora sequi tempore saepe similique quod in fuga libero quia, suscipit, incidunt voluptatum officiis vero optio perferendis fugiat ducimus. Reprehenderit necessitatibus repudiandae eos nulla hic dolorum voluptatem maiores odio eaque sequi, temporibus, quasi praesentium et, ea aliquam, repellat omnis quo! Vel quam aliquid eius maiores autem, id consectetur placeat debitis suscipit error. Placeat iste, quibusdam error, iusto nemo provident. Minus rem reiciendis ipsa excepturi sapiente adipisci beatae atque veritatis omnis libero fuga assumenda similique provident, molestiae consequatur totam voluptates ducimus deleniti pariatur quo labore eos quia delectus. Totam molestiae perspiciatis optio numquam quibusdam quis eaque quia vel iste, aspernatur. Fugit, soluta qui eligendi asperiores reprehenderit repudiandae reiciendis quidem. Architecto earum corporis suscipit fugit minima ipsum, quibusdam praesentium ab numquam amet! Mollitia quidem cum modi nesciunt nemo? Quas nihil distinctio nulla dicta placeat minima, necessitatibus minus doloremque, temporibus, animi, odio autem modi quia possimus repudiandae tenetur sed impedit dolore explicabo accusantium ipsa! Doloribus rem ipsam asperiores quas, dignissimos hic deleniti. Eaque provident minus eos officia perferendis aliquid impedit sequi modi non hic inventore unde dolor, necessitatibus numquam ratione voluptate, debitis in pariatur obcaecati! Aliquid autem vitae facilis porro repellat, error doloremque alias perspiciatis, possimus dicta? Vero debitis omnis voluptates odio repudiandae in libero sequi dolore repellat laborum quidem quisquam facere veniam sapiente vel aliquid minus commodi sint, perspiciatis fugit magni voluptate. Iusto nesciunt reiciendis, et consequatur repellendus non provident, quam amet quidem ipsa adipisci eos quas facilis ex sapiente, dolore quisquam magnam omnis explicabo. Odio soluta laboriosam recusandae. Iusto nobis repellendus, distinctio ab aliquid tempora saepe neque deserunt tempore ut fuga, quis totam facere et temporibus? Aliquid ipsum, rerum placeat odit? Omnis provident fugiat repellat nisi aut quae itaque deleniti, culpa unde officiis nobis adipisci quos.
+        <ul>
+          <li>
+            - Разработка дизайна, подбор цветовой темы и создание макета
+          </li>
+          <li>
+            - Копирайтинг текста для каждой страницы на сайте
+          </li>
+          <li>
+            - Подбор и обработка изображений
+          </li>
+          <li>
+            - Разработка отзывчивого адаптивного сайта
+          </li>
+          <li>
+            - SEO-оптимизация страниц сайта
+          </li>
+        </ul>
       </section>
     </article>
   </main>
