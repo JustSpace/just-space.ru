7f0ecf5f010d1b03494f8c0281511967be6b6472
Кирилл Анисимов <akpoflash@MacBook-Pro-Kirill.local>
2017-01-06 16:49:32 +0300

    Fix mistake with meta tags

--- templates/doc_head.php
+++ templates/doc_head.php
@@ -8,8 +8,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta description="<?php echo $meta->GetDescription(); ?>">
-  <meta keywords="<?php echo $meta->GetKeywords(); ?>">
+  <meta name="description" content="<?php echo $meta->GetDescription(); ?>">
+  <meta name="keywords"  content="<?php echo $meta->GetKeywords(); ?>">
   <title><?php echo $meta->GetTitle(); ?></title>
   <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="/css/styles.min.css">
