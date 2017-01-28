bda683e0fc5619b5a0c079db92bf8e35c2063802
Кирилл Анисимов <akpoflash@MacBook-Pro-Kirill.local>
2017-01-28 21:49:10 +0300

    Create tools folder and add template page

--- tools.php
+++ tools.php
@@ -21,6 +21,18 @@
   <main class="main">
     <article class="tools">
       <div class="tool">
+        <h2 class="tool__title">Framework для прототипирования</h2>
+        <div class="tool__text">
+          Мы создали свой framework для возможности быстро и удобно прототипировать сайты для показа рабочих шаблонов заказчикам.
+        </div>
+        <a href="#" class="button button--underline button--arrow">
+          Попробовать
+          <svg class="article__icon" aria-hidden="true">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/sprites/sprites.svg#next-arrow"></use>
+          </svg>
+        </a>
+      </div>
+      <div class="tool">
         <h2 class="tool__title">Проверка позиций в поисковой выдаче</h2>
         <div class="tool__text">
           С помощью нашего инструмента вы можете быстро проверить текущие позиции сайта в поисковой выдачи, по любым интересующим вас запросам.
@@ -33,21 +45,9 @@
         </a>
       </div>
       <div class="tool">
-        <h2 class="tool__title">Аудит поисковой оптимизации сайта</h2>
+        <h2 class="tool__title">Умная Email рассылка</h2>
         <div class="tool__text">
-          Аудит сайта позволят находить узкие места в сайте, что дает возможность в короткие сроки исправить проблемы и улучшать свои позиции в поисковой выдаче.
-        </div>
-        <a href="#" class="button button--underline button--arrow">
-          Попробовать
-          <svg class="article__icon" aria-hidden="true">
-            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/sprites/sprites.svg#next-arrow"></use>
-          </svg>
-        </a>
-      </div>
-      <div class="tool">
-        <h2 class="tool__title">Framework для прототипирования</h2>
-        <div class="tool__text">
-          Мы создали свой framework для возможности быстро и удобно прототипировать сайты для показа рабочих шаблонов заказчикам.
+          Мы разработили инструмент позволяющий эффективно информировать постоянных клиентов, и легко привлекать новых.
         </div>
         <a href="#" class="button button--underline button--arrow">
           Попробовать
